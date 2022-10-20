90dd20c010b8c8443817cdec61c8fc3654a6cb1e
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-20 16:39:28 -0300

    feat(read): requisito 01 - carregar arquivo csv / listar os dados no formato list[dict]

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,9 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
-def read(path):
+def read(path: str) -> list[dict]:
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -15,4 +16,18 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+    with open(path, encoding="utf-8") as file:
+        jobs = csv.reader(file, delimiter=",", quotechar='"')
+        head, *tail = jobs
+
+    # usando list comprehension
+    return [dict(zip(head, job)) for job in tail]
+
+    # usando for
+    # result = list()
+
+    # for job in tail:
+    #     result.append(dict(zip(head, job)))
+
+    # return result

ed7a030639c136015af8603af4bda245f9b4257f
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-21 19:23:15 -0300

    feat(flask): requisito bonus - implementação da rota /job/<index> lista um unico job pelo seu id

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -12,6 +12,7 @@ from .insights import (
 )
 
 from .more_insights import (
+    get_job,
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
@@ -71,7 +72,14 @@ def list_jobs():
         "max_salary": get_max_salary("src/jobs.csv"),
     }
 
-    return render_template("list_jobs.jinja2", ctx=ctx)
+    return render_template("list_jobs.jinja2", ctx=ctx), 200
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs: list[dict] = read(path="src/jobs.csv")
+    job: dict | None = get_job(jobs, index)
+    return render_template("job.jinja2", job=job), 200
 
 
 def init_app(app: Flask):

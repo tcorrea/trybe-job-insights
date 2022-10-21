db75f7b136692e319bb9882fc94c524074f699e7
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-21 18:01:13 -0300

    test(brazilian_jobs): requisito 11 - criado teste para a função brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+import pytest
 
 
-def test_brazilian_jobs():
-    pass
+@pytest.fixture
+def fake_expected_data():
+    return {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+
+
+# @pytest.fixture
+# def fake_data():
+#     return {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}
+
+
+def test_brazilian_jobs(fake_expected_data):
+    expected_data = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    assert expected_data[0] == fake_expected_data
+
+    # TODO: mock a função jobs.read que é chamada no read_brazilian_file

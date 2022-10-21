564e93e23e4e2b6943b038436d1907d66f14e3a4
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-21 15:54:43 -0300

    test(counter): requisito 10 - implementar função para count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,24 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+import pytest
+from unittest.mock import mock_open, patch
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def fake_data():
+    return "javascript JAVASCRIPT PYTHON python pythom javascrip"
+
+
+EXPECTED_JS_OCURRENCES = 2
+EXPECTED_PY_OCURRENCES = 2
+
+
+def test_counter(fake_data):
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("", "")
+
+    with patch("builtins.open", mock_open(read_data=fake_data)):
+        javascript_ocurrences = count_ocurrences("", "javascript")
+        python_ocurrences = count_ocurrences("", "python")
+
+    assert javascript_ocurrences == EXPECTED_JS_OCURRENCES
+    assert python_ocurrences == EXPECTED_PY_OCURRENCES

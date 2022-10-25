ec3c55aa3286a4642fae7c290b12f6230884cb4e
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-25 18:27:15 -0300

    adicioando referencia

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -13,9 +13,11 @@ def fake_data():
     return [{"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}]
 
 
-# NOTE: a função "read_brazilian_file" usa a função "read" do modulo "jobs"
+# NOTE: A função "read_brazilian_file" usa a função "read" do modulo "jobs",
 # NOTE: por isso que tentar mockar a função "read" no modulo
-# NOTE: read_brazilian_file estava dando erro
+# NOTE: read_brazilian_file estava dando erro.
+# NOTE: Mock da função "read" tem que ser passado antes das fixtures. Ref:
+# https://stackoverflow.com/questions/25057383/patch-decorator-is-not-compatible-with-pytest-fixture
 @patch("src.jobs.read")
 def test_brazilian_jobs(read_mock, fake_expected_data, fake_data):
     read_mock.return_value = fake_data

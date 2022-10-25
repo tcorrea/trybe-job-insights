8edc98520bdc3f14f525b8c6301f235a0f217458
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-10-25 18:18:15 -0300

    refactor(test_brazilian_jobs): criado o mock ta função "read" que era usado para realizar os testes

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,20 +1,24 @@
 from src.brazilian_jobs import read_brazilian_file
 import pytest
+from unittest.mock import patch
 
 
 @pytest.fixture
 def fake_expected_data():
-    return {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    return [{"title": "Maquinista", "salary": "2000", "type": "trainee"}]
 
 
-# @pytest.fixture
-# def fake_data():
-#     return {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}
+@pytest.fixture
+def fake_data():
+    return [{"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}]
 
 
-def test_brazilian_jobs(fake_expected_data):
-    expected_data = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+# NOTE: a função "read_brazilian_file" usa a função "read" do modulo "jobs"
+# NOTE: por isso que tentar mockar a função "read" no modulo
+# NOTE: read_brazilian_file estava dando erro
+@patch("src.jobs.read")
+def test_brazilian_jobs(read_mock, fake_expected_data, fake_data):
+    read_mock.return_value = fake_data
+    expected_data = read_brazilian_file("any-path")
 
-    assert expected_data[0] == fake_expected_data
-
-    # TODO: mock a função jobs.read que é chamada no read_brazilian_file
+    assert expected_data == fake_expected_data
